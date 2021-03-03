4a6f700e720f85dded15dc02917ce3f3b69b34ec
NIK-TIGER-BILL <nik.tigr2010@gmail.com>
2021-03-04 00:55:01 +0300

    deploy bot!

--- homework.py
+++ homework.py
@@ -8,6 +8,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+logging.basicConfig(
+        level=logging.DEBUG,
+        filename='main.log',
+        format='%(asctime)s, %(levelname)s, %(message)s, %(name)s')
+logger = logging.getLogger('__name__')
+logger.setLevel(logging.INFO)
+handler = RotatingFileHandler('my_logger.log', maxBytes=50000000,
+                              backupCount=5)
+logger.addHandler(handler)
+
 API_YANDEX_URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
@@ -16,11 +26,17 @@ CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 def parse_homework_status(homework):
     homework_name = homework.get('homework_name')
-    if homework.get('status') == 'rejected':
+    homework_status = homework.get('status')
+    if homework_name is None or homework_status is None:
+        return 'Пришли пустые данные, ошибка API'
+    if homework_status == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
+    elif homework_status == 'approved':
+        verdict = ('Ревьюеру всё понравилось, '
+                   'можно приступать к следующему уроку.')
     else:
-        verdict = 'Ревьюеру всё понравилось, ' \
-                  'можно приступать к следующему уроку.'
+        return f'Работа {homework_name} взята на проверку'
+
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
@@ -37,15 +53,6 @@ def send_message(message, bot_client):
 
 
 def main():
-    logging.basicConfig(
-        level=logging.DEBUG,
-        filename='main.log',
-        format='%(asctime)s, %(levelname)s, %(message)s, %(name)s')
-    logger = logging.getLogger('__name__')
-    logger.setLevel(logging.INFO)
-    handler = RotatingFileHandler('my_logger.log', maxBytes=50000000,
-                                  backupCount=5)
-    logger.addHandler(handler)
     current_timestamp = int(time.time())
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     logging.debug('Бот запущен')
@@ -61,7 +68,7 @@ def main():
                                                  current_timestamp)
             time.sleep(300)
         except Exception as e:
-            logger.exception()
+            logger.exception('')
             send_message(f'Бот столкнулся с ошибкой: {e}', bot)
             time.sleep(5)
 
